9e0119a694036acaf83e639433562bb6c5cb53a8
rafaScalet <rafaelscalet2018@gmail.com>
2024-04-20 11:59:42 -0300

    how does a bitwise operation works

--- src/main/Main.java
+++ src/main/Main.java
@@ -2,6 +2,11 @@ package main;
 
 public class Main {
     public static void main(String[] args) {
-
+        int num = 1; //bit: 00000000000000000000000000000001
+        System.out.println(num);
+        num = num << 1; //bit: 00000000000000000000000000000010
+        System.out.println(num);
+        num = num << 2; //bit: 00000000000000000000000000001000
+        System.out.println(num);
     }
 }
