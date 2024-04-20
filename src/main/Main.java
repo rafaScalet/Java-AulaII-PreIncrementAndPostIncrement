dcc993c464a5b5728816faf9eb3ecfad1f8ea4f7
rafaScalet <rafaelscalet2018@gmail.com>
2024-04-20 12:14:57 -0300

    pre and post operation

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,12 +1,25 @@
 package main;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        int num = 1; //bit: 00000000000000000000000000000001
-        System.out.println(num);
-        num = num << 1; //bit: 00000000000000000000000000000010
-        System.out.println(num);
-        num = num << 2; //bit: 00000000000000000000000000001000
-        System.out.println(num);
+        int num1, num2;
+        Scanner in = new Scanner(System.in);
+
+        System.out.print("Enter the first value: ");
+        num1 = in.nextInt();
+
+        System.out.print("Enter the second value: ");
+        num2 = in.nextInt();
+
+        in.close();
+
+        System.out.println("\nConcatenation: " + num1 + num2);
+        System.out.println("Sum: " + (num1 + num2));
+        System.out.println("Sum (Post Operation): " + (num1++ + num2));
+        System.out.println("Concatenation: " + num1 + num2);
+        System.out.println("Sum (Pre Operation): " + (++num1 + num2));
+        System.out.println("Concatenation: " + num1 + num2);
     }
 }
